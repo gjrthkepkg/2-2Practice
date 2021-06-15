7fd51a3265818430886cff6d9d79b06dc29019a5
澤淵 徐 <vi49th161224@gmail.com>
2021-06-15 15:42:00 +0800

    2-2

--- 2-2Practice/Default.aspx.cs
+++ 2-2Practice/Default.aspx.cs
@@ -7,8 +7,31 @@ using System.Web.UI.WebControls;
 
 namespace _2_2Practice {
     public partial class Default : System.Web.UI.Page {
-
         protected void Page_Load(object sender, EventArgs e) {
+            int year = 30;
+            bool isleap = false;
+
+            if (year % 4 == 0){
+                isleap = true;
+
+                if (year % 100 == 0){
+                    if (year % 400 == 0){
+                        isleap = true;
+                    }
+                    else
+                    {
+                        isleap = false;
+                    }
+                }
+            }
+            if (isleap == true)
+            {
+                Response.Write("Y");
+            }
+            else
+            {
+                Response.Write("N");
+            }
         }
     }
 }
